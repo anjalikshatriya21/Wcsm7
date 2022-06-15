715990368c24fe21634cfe30a54159c0119c1a22
ankit <ankit@LAPTOP-4JD4S4RV>
2022-06-15 09:37:32 +0530

    First comment

---
+++ TestActiTimeFramework/src/generic/BaseTest.java
@@ -0,0 +1,23 @@
+package generic;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest implements IAutoConstant{
+    static WebDriver driver;
+    public void openBrowser()	
+    {
+    	Flib flib = new Flib();
+    	
+    	
+    	
+    }
+    	
+    	
+    	
+    
+    {   
+ 
+     
+     
+    }
+}
